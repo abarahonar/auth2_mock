0126a460593011ad5ab3396c5bac27149fbc48ad
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2021-03-03 19:06:48 -0300

    Agregado reconocimiento de usuario

--- front_mock/src/App.vue
+++ front_mock/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="tm-2 container has-text-centered">
+    <div v-if="!loading">
+      <h1 v-if="user == ''" class="title mt-6">Lo sentimos pero no hay un usuario autenticado en este momento en este momento</h1>
+      <h1 v-else class="title mt-6">Ahora mismo, {{user}} se encuentra autenticado</h1>
+    </div>
     <button
       class="my-6 mx-6 button is-rounded is-success is-light"
       @click="login"
@@ -24,6 +28,25 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      user: "",
+      loading: true
+    }
+  },
+  async mounted() {
+    let res = await fetch("https://back.catteam.tk/verify", {
+      method: "get",
+      credentials: 'include'
+    });
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+      this.user = data.name;
+      console.log(this.user);
+    }
+    this.loading = false;
+  },
   methods: {
     login: () => {
       window.location.href = "https://auth.catteam.tk?origin=https://mock.catteam.tk "
